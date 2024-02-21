fe9b82c785ac832417b802240beb090f650c7880
Jack Liu <yliu863@uottawa.ca>
2024-02-20 22:46:47 -0500

    test

--- ColorImage.java
+++ ColorImage.java
@@ -49,7 +49,6 @@ public class ColorImage {
     }
 
     public void reduceColor(int d) {
-    }
+	}
 
-   
 }
--- SimilaritySearch.java
+++ SimilaritySearch.java
@@ -1,17 +1,82 @@
-import java.util.List;
+//YuYang Liu 300299216 and Wajeeh Syed 300299214
 
 public class SimilaritySearch {
-    public double compare(ColorImage img1, ColorImage img2) {
-        // Compare two images and return a similarity score
-        return 0.0; // Placeholder
+    private ColourImage queryImage;
+    private ColourHistogram queryHistogram;
+    private ArrayList<ColourHistogram> dataset = new ArrayList<>();
+    private PriorityQueue<Pair<Double, ColourHistogram>> intersections;
+    private int depth;
+    private Boolean error = false;
+
+    public SimilaritySearch(String filename, String directory, int depth) {
+        this.depth = depth;
+        File dir = new File(directory);
+        File[] directoryListing = dir.listFiles();
+
+        if (directoryListing != null) {
+            for (File image : directoryListing) {
+                if (image.getName().contains("txt")) {
+                    // Do something
+                } else {
+                    // Do something else
+                }
+                try {
+                    String fullName = directory + "/" + image.getName();
+                    ColourHistogram histogram = new ColourHistogram(fullName);
+                    histogram.normalize();
+                    dataset.add(histogram);
+                } catch (Exception e) {
+                    System.out.println("Unable to create histogram for " + image.getName());
+                    System.out.println("Invalid Directory.");
+                    error = true;
+                }
+            }
+            try {
+                queryImage = new ColourImage(filename);
+                queryImage.reduceColour(depth);
+            } catch (Exception e) {
+                System.out.println("Unable to read query image.");
+                error = true;
+            }
+        }
     }
 
-    public List<ColorImage> findSimilarImages(ColorImage target, List<ColorImage> imageList) {
-        // Find and return a list of images similar to the target image
-        return null; // Placeholder
+    public void search() {
+        if (!error) {
+            queryHistogram = new ColourHistogram(queryImage, depth);
+            queryHistogram.normalize();
+        }
+
+        if (!error) {
+            intersections = new PriorityQueue<>(6, new PairComparator());
+            Iterator<ColourHistogram> iter = dataset.iterator();
+
+            while (iter.hasNext()) {
+                ColourHistogram histogram = iter.next();
+                double intersection = queryHistogram.compare(histogram);
+                Pair<Double, ColourHistogram> entry = new Pair<>(intersection, histogram);
+                intersections.add(entry);
+
+                if (intersections.size() == 6) {
+                    intersections.poll();
+                }
+            }
+
+            Iterator<Pair<Double, ColourHistogram>> iter2 = intersections.iterator();
+
+            while (iter2.hasNext()) {
+                Pair<Double, ColourHistogram> current = iter2.next();
+                ColourHistogram histogram = current.getValue();
+                System.out.println(current.getKey() + "" + histogram.getFilename());
+            }
+        }
     }
-    
-    public static void main(String[] args) {
-        
+
+    public ColourHistogram getHistogram() {
+        return queryHistogram;
+    }
+
+    public ColourImage getImage() {
+        return queryImage;
     }
 }
